cdd22ae75726a9acc440e6d7a59da6735df8f394
adroitanuj <134070793+adroit-anuj@users.noreply.github.com>
2024-06-23 15:33:05 +0530

    Update ChatApp.cpp

--- ChatApp.cpp
+++ ChatApp.cpp
@@ -1,20 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void page1();
+void homepage();
 void page2(string);
 map<string,string> user;
 
-class frnd
-{
+
+
+
+
+class hierarchy{
+
+protected:
+    
     vector<string> flist;
-    struct chat
-    {
-    int message_id;
-    string sender,receiver;
-    char mess[50][100];
-    int num_of_message=0;
+    struct chat{
+        int message_id;
+        string sender,receiver;
+        char mess[50][100];
+        int num_of_message=0;
     };
+};
+
+
+class author : public hierarchy{
+public:
+    void erase_data(){
+        flist.clear();
+    }
+};
+
+
+
+class frnd : public hierarchy 
+{
+
 
 public:
     map<string,chat> sms;
@@ -85,11 +105,10 @@ void frnd::addfrnd(string uname)
  cout<<"\n Enter the name which you want to add in your friend list"<<endl;
  cin>>addname;
  cout<<"\n Please wait! checking for  "<<addname<<"......\n";
- // waiting time for new mwssage
-for(int i=0;i<99999910;)
-{
-i++;
-}
+
+
+for(int i=0;i<99999910;)i++;//Buffering... :D
+
  for(iuser=user.begin();iuser!=user.end();iuser++)
         {
             for(iv=flist.begin();iv!=flist.end();iv++)
@@ -110,7 +129,7 @@ i++;
              flag++;
 
 }
-//error for trying to add yourself as yourcle friend
+
 if(uflag==1 && flag==0 && flag3==0)
     cout<<"\nSorry! Can't add yourself to your friend list\n";
 if(flag==1 && uflag==0 && flag3==0)
@@ -184,106 +203,86 @@ void frnd::unfrnd(string uname)
 }
 }
 
-void page1()
+void homepage()
 {
-    int flag=0,choice;
+        int flag=0,choice;
 
-cout<<"\n\t\t\t WELCOME TO THE CHATAPP\n";
-
-
-
-
-    while(1)
-{
-
-cout<<"\n Please Enter your choice\n  1-Sign_up\n  2-Sign_in\n  3-Exit \n \n"<<endl;
-cin>>choice;
-
-switch(choice)
-{
-case 1:
+    cout<<"\n\t\t\t WELCOME TO THE CHATAPP\n";
+    while(true)
     {
 
-        string username,pwd;
-        cout<<"\n Choose a username\n";
-        cin>>username;
-        cout<<"\n Choose a password\n";
-        cin>>pwd;
-        map<string,string>::iterator iuser;
-        if(user.empty())
-        {
-        user[username]=pwd;
-        }
-        else
-        {
+        cout<<"\n Please Enter your choice\n  1-Sign_up\n  2-Sign_in\n  3-Exit \n \n"<<endl;
+        cin>>choice;
 
-        for(iuser=user.begin();iuser!=user.end();iuser++)
+        switch(choice)
         {
-            if(iuser->first==username)
+        case 1:
             {
-                cout<<"\n Sorry! username not available..please try with some other name\n";
+
+                string username,pwd;
+                cout<<"Choose a username: ";
+                cin>>username;cout<<endl;
+                cout<<"Choose a password: ";
+                cin>>pwd;cout<<endl;cout<<"Successful!\n";
+
+                map<string,string>::iterator iuser;
+                if(user.empty())
+                    user[username]=pwd;
+                else{
+                    for(iuser=user.begin();iuser!=user.end();iuser++){
+                        if(iuser->first==username){
+                            cout<<"\n Sorry! username is not available..please try with some other name\n";
+                            break;
+                        }
+                        else{
+                            user[username]=pwd;
+                          break;
+                        }
+                    }
+                }
+            break;
+            }
+
+
+         case 2:{
+
+                string lusername,lpwd;
+                cout<<"\n Enter your username\n";
+                cin>>lusername;
+                cout<<"\n Enter your password\n";
+                cin>>lpwd;
+                map<string,string>::iterator iuser;
+                for(iuser=user.begin();iuser!=user.end();iuser++){
+                    if(iuser->first==lusername && iuser->second==lpwd){
+                        cout<<"\n WELCOME "<<lusername<<"!!  You have successfully logged in \n";
+                        flag=1;
+                        page2(lusername);
+                        break;
+                    }
+                }
+                    if(flag==0){
+                        cout<<"\nusername or password incorrect\n\t!!Please try again\n";
+                        break;
+                    }
+
                 break;
             }
-            else
-              {user[username]=pwd;
-              break;}
+
+
+        case 3:{
+
+                cout<<"\ndisplay\n";
+                map<string,string>::iterator iuser;
+                cout<<"\nall users\n";
+                for(iuser=user.begin();iuser!=user.end();iuser++){
+                    cout<<iuser->first<<"\t"<<iuser->second;
+                }break;
+            }
         }
+        if(flag==1){
+            break;
         }
-    break;
     }
-
-
- case 2:
-    {
-
-        string lusername,lpwd;
-        cout<<"\n Enter your username\n";
-        cin>>lusername;
-        cout<<"\n Enter your password\n";
-        cin>>lpwd;
-        map<string,string>::iterator iuser;
-        for(iuser=user.begin();iuser!=user.end();iuser++)
-        {
-            if(iuser->first==lusername && iuser->second==lpwd)
-            {
-                    cout<<"\n WELCOME "<<lusername<<"!!  You have successfully logged in \n";
-
-                    flag=1;
-                    page2(lusername);
-                    break;
-
-            }}
-
-
-            if(flag==0)
-                {
-                    cout<<"\nusername or password incorrect\n\t!!Please try again\n";
-                    break;
-                }
-
-break;
-        }
-
-
-case 3:
-    {
-
-        cout<<"\ndisplay\n";
-        map<string,string>::iterator iuser;
-        cout<<"\nall users\n";
-for(iuser=user.begin();iuser!=user.end();iuser++)
-{
-    cout<<iuser->first<<"\t"<<iuser->second;
-}
-        break;
-    }
-
-}
-if(flag==1)
-                {
-                    break;
-                }
-}
 }
 
 
@@ -293,7 +292,7 @@ void page2(string uname)
    
 int choice2,choice3;
 main_menu:
-    cout<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl<<endl;
+    cout<<endl<<endl<<endl<<endl<<endl;
     cout<<"\t\t\t     WELCOME    "<<uname<<endl<<endl;
    
     cout<<"\n\t    ***********Choose from following menu**********\n";
@@ -301,132 +300,99 @@ main_menu:
     cin>>choice2;
     switch(choice2)
     {
-    case 1:
-      {
-
+        case 1:{
             f[uname].inbox(uname);
- cout<<"\n press any number except '0' to go back to main menu\n";
-        cin>>choice3;
-        if(choice3)
-        goto main_menu;
-
-break;
-        }
-    case 2:
-        {
-            string reciepient;
-            cout<<"\n Please enter the reciepient's name:\n";
-            cin>>reciepient;
-            char message[100];
-        cout<<"\n Please Type Your message :"<<endl;
-        cin.ignore();
-        cin.getline(message,100);
-            f[reciepient].chat_on(uname,reciepient,message);
-            cout<<"\n Your message has been sent\n";
             cout<<"\n press any number except '0' to go back to main menu\n";
-        cin>>choice3;
-        if(choice3)
-        goto main_menu;
+            cin>>choice3;
+            if(choice3)
+            goto main_menu;
 
-break;
+            break;
+        }
+        case 2:{
+                string reciepient;
+                cout<<"\n Please enter the reciepient's name:\n";
+                cin>>reciepient;char message[100];
+                cout<<"\n Please Type Your message :"<<endl;
+                cin.ignore();cin.getline(message,100);
+                f[reciepient].chat_on(uname,reciepient,message);
+                cout<<"\n Your message has been sent\n";
+                cout<<"\n press any number except '0' to go back to main menu\n";
+                cin>>choice3;
+                if(choice3)goto main_menu;
+            break;
 
         }
-    case 3:
-        {
-           f[uname].dispfrnd(uname);
-           cout<<"\n Press any number except '0' to go back to main menu\n";
-        cin>>choice3;
-        if(choice3)
-        goto main_menu;
-      break;
-       }
-    case 4:
-        {
-        f[uname].addfrnd(uname);
-        cout<<"\n Press any number except '0' to go back to main menu\n";
-        cin>>choice3;
-if(choice3)
-        goto main_menu;
-        break;
+        case 3:{
+               f[uname].dispfrnd(uname);
+               cout<<"\n Press any number except '0' to go back to main menu\n";
+                cin>>choice3;
+                if(choice3)goto main_menu;
+                break;
         }
-    case 5:
-        {
-        f[uname].unfrnd(uname);
-        cout<<"\n Press any number except '0' to go back to main menu\n";
-        cin>>choice3;
-if(choice3)
-        goto main_menu;
-        break;}
+        case 4:{
+            f[uname].addfrnd(uname);
+            cout<<"\n Press any number except '0' to go back to main menu\n";
+            cin>>choice3;
+            if(choice3)goto main_menu;
+            break;
+        }
+        case 5:{
+            f[uname].unfrnd(uname);
+            cout<<"\n Press any number except '0' to go back to main menu\n";
+            cin>>choice3;
+            
+            if(choice3)goto main_menu;
+            break;
+        }
+        case 6:{
+            char ch;
+            vector<string>rec;
+            map<string,string>::iterator iuser;
+            vector<string>::iterator irec;
+            group:
+            int flag=0;
+            string reciepient;
+            cout<<"Enter reciepient";
+            cin>>reciepient;
 
-
-    case 6:
-        {
-
-    char ch;
-
-    vector<string>rec;
-    map<string,string>::iterator iuser;
-    vector<string>::iterator irec;
-group:
-    int flag=0;
-        string reciepient;
-    cout<<"Enter reciepient";
-    cin>>reciepient;
-
-    for(iuser=user.begin();iuser!=user.end();iuser++)
-        {
-            if(iuser->first==reciepient)
-            {
-                flag++;
+            for(iuser=user.begin();iuser!=user.end();iuser++){
+                if(iuser->first==reciepient)flag++;
             }
+            if(!flag){
+                cout<<"\n Sorry!! No such user exist\a\a\n";
+                goto group;
+            }
+
+            rec.push_back(reciepient);
+            cout<<"Do you want to add more reciepients (y/n)?\n";
+            cin>>ch;
+
+            if(ch=='y')goto group;
+            else if(ch=='n'){
+                char message[100];
+                cout<<"\nPlease Type Your message :"<<endl;
+                cin.ignore();
+                cin.getline(message,100);
+                for(irec=rec.begin();irec!=rec.end();irec++)f[*irec].chat_on(uname,*irec,message);
+            }
+            cout<<"\n Your message has been sent\n";
+            cout<<"\n Press any number except '0' to go back to main menu\n";
+            cin>>choice3;
+            if(choice3)goto main_menu;
+            break;
         }
-        if(!flag)
-        {
-            cout<<"\n Sorry!! No such user exist\a\a\n";
-            goto group;
-        }
 
-    rec.push_back(reciepient);
-    cout<<"Do you want to add more reciepients (y/n)?\n";
-    cin>>ch;
-
-    if(ch=='y')
-        goto group;
-
-    else if(ch=='n')
-    {
-         char message[100];
-        cout<<"\nPlease Type Your message :"<<endl;
-        cin.ignore();
-        cin.getline(message,100);
-         for(irec=rec.begin();irec!=rec.end();irec++)
-        {
-            f[*irec].chat_on(uname,*irec,message);
+        case 7:{
+            system("cls");
+            cout<<"\n You have been successfully logged out\n";
+            homepage();
+            break;
         }
     }
-    cout<<"\n Your message has been sent\n";
-    cout<<"\n Press any number except '0' to go back to main menu\n";
-        cin>>choice3;
-if(choice3)
-        goto main_menu;
-        break;
-        }
-
-case 7:
-    {
-        system("cls");
-        cout<<"\n You have been successfully logged out\n";
-
-        page1();
-
-
-        break;
-
-    }
-}
 }
 
 
 
 
-int main(){page1();return 0;}
+int main(){homepage();return 0;}
